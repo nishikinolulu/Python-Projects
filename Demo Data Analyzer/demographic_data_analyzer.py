9a861206b7c55cb10b6251f0f30c9d48ad52115c
nishikinolulu <111897072+nishikinolulu@users.noreply.github.com>
2024-03-21 20:26:14 -0700

    Update demographic_data_analyzer.py
    Redundant path variable removed

--- Demo Data Analyzer/demographic_data_analyzer.py
+++ Demo Data Analyzer/demographic_data_analyzer.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    path = "/c/Users/lmcmu/Desktop/Coding Projects/Python-Projects/Demo Data Analyzer/"
     df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
